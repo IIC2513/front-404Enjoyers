b1cc3a7ea118802e1882a28e870e62de625dd5dc
NICOLAS ARAYA <nicolasaaat@gmail.com>
2024-11-12 01:57:08 -0300

    basic jwt handling, connection with backend

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,31 @@
+import { useContext, useEffect, useState } from 'react';
 import './../assets/styles/style.css'
 import { Routes, Route, Link } from 'react-router-dom'
-
+import axios from 'axios';
+import { AuthContext } from './auth/AuthContext';
+import LogoutButton from './auth/logout';
 
 function Navbar(){
+  const {token} = useContext(AuthContext);
+  const [msg, setMsg] = useState("");
+  const config = {
+    'method': 'get',
+    'url': `${import.meta.env.VITE_BACKEND_URL}/users/1/show`,
+    'headers': {
+      'Authorization': `Bearer ${token}`
+    }
+  }
+
+  useEffect(() => {
+    axios(config).then((response) => {
+      console.log("Showing credentials because you are logged");
+      console.log(response);
+      setMsg(`Welcome, ${response.data.user.username} (${response.data.user.email})`);
+    }).catch((error) => {
+      console.log("An error ocurred. You are not logged.");
+      console.log(error);
+    })
+  }, [])
   return (
     <>
         <nav>
@@ -17,6 +40,8 @@ function Navbar(){
               <li><Link to="/matches">Ir a Partida</Link></li>
             </ul>
         </nav>
+        <h3>{msg}</h3>
+        <LogoutButton></LogoutButton>
     </>
   );
 }
