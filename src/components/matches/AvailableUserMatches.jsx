a240f62f4a06499a44af1caa7329f9633eb627f3
NICOLAS ARAYA <nicolasaaat@gmail.com>
2024-11-20 23:17:28 -0300

    table for matches already in

--- src/components/matches/AvailableUserMatches.jsx
+++ src/components/matches/AvailableUserMatches.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { getUserMatches } from './MatchService';
+import { getUserMatches, getUsers} from './MatchService';
 import { getBoardDetails } from '../board/BoardServices';
 import { AuthContext } from '../auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 function AvailableUserMatches({ userId }) {
     const [matches, setMatches] = useState([]);
+    const [users, setUsers] = useState([]);
     const {token} = useContext(AuthContext);
     const navigate = useNavigate();
 
@@ -14,6 +15,14 @@ function AvailableUserMatches({ userId }) {
             try{
                 const response = await getUserMatches(userId, token);
                 setMatches(response.matches || []);
+
+                const usersPromises = response.matches.map(async (match) => {
+                    const usersResponse = await getUsers(match.id, token);
+                    return usersResponse.users;
+                });
+
+                const usersForMatches = await Promise.all(usersPromises);
+                setUsers(usersForMatches);
             }catch(error){
                 alert("Error at load user matches: " + error.message);
             }
@@ -22,6 +31,20 @@ function AvailableUserMatches({ userId }) {
     }, []);
 
     const handleMatchClick = async (match) => {
+        try {
+            const matchDetails = await getMatchDetails(match.id, token);
+            if (matchDetails) {
+                console.log(`Navigating to match with ID: ${match.id}`);
+               navigate(`/matches/${match.id}`);
+            } else {
+                alert("Match not found.");
+            }
+        } catch (error) {
+            alert("Error retrieving match details: " + error.message);
+        }
+    };
+
+    const handlePlayClick = async (match) => {
 
         try {
             const boardDetails = await getBoardDetails(match.id, token)
@@ -39,14 +62,50 @@ function AvailableUserMatches({ userId }) {
     return (
         <div>
             <h2>Matches you are already in</h2>
-            <ul>
-                {matches.map((match) => (
-                    <li key={match.id} 
-                        onClick={() => handleMatchClick(match)}
-                        style={{ cursor: 'pointer', color: match.public ? 'black' : 'red' }}
-                        >ID: {match.id} ({match.status})</li>
-                ))}
-            </ul>
+            <div className="matches-table">
+            <div className="matches-header">
+            <div>Match ID</div>
+            <div>Status</div>
+            <div>Players</div>
+            <div>Turns</div>
+            <div>Actions</div>
+            </div>
+            {matches.map((match, index) => (
+            <div key={match.id} className="matches-row">
+                <div>{match.id}</div>
+                <div>{match.status}</div>
+                <div>{users[index]?.length}</div>
+                <div>{match.currentTurn}/{match.turns}</div>
+                <div className="actions">
+                {match.status === 'waiting' && (
+                    <>
+                    <button
+                        onClick={(e) => {
+                        e.stopPropagation();
+                        handleMatchClick(match);
+                        }}
+                    >
+                        View
+                    </button>
+                    </>
+                )}
+                {match.status === 'in_progress' && (
+                    <>
+                    <button
+                        className='playBtn'
+                        onClick={(e) => {
+                        e.stopPropagation();
+                        handlePlayClick(match);
+                        }}
+                    >
+                        Play
+                    </button>
+                    </>
+                )}
+                </div>
+            </div>
+            ))}
+        </div>
         </div>
     );
 }
